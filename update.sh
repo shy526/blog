a35012f73401ceb74e96668059c524182e921c31
ccxh <sundada214@outlook.com>
2018-02-15 12:19:04 +0800

    工具页面添加一个http状态码并前端支持导出功能

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
-#git fetch https://github.com/sunjiaqing/blog.git
+#!/bin/bash
+echo "拉取更新"
+echo "--------------------------------------------"
 git pull
+echo "清理"
 mvn clean
+echo "重新打包"
 mvn install
