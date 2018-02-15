d3aa76a42921df61366d9634f40f39d96a3498c2
ccxh <sundada214@outlook.com>
2018-02-15 12:35:47 +0800

    测试

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-BLOG-API-PID=`ps -ef | grep blog-api.jar  | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
-BLOG-SCHEDULER-PID=`ps -ef | grep blog-scheduler-1.0-SNAPSHOT.jar  | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
-echo "blog-api.jar pid : " $BLOG-API-PID
-echo " blog-scheduler-1.0-SNAPSHOT.jar : " $BLOG-SCHEDULER-PID
+BLOG_API_PID=`ps -ef | grep blog-api.jar  | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
+BLOG_SCHEDULER_PID=`ps -ef | grep blog-scheduler-1.0-SNAPSHOT.jar  | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
+echo "blog-api.jar pid : "$BLOG_API_PID
+echo " blog-scheduler-1.0-SNAPSHOT.jar : "$BLOG_SCHEDULER_PID
 echo "-----------------------"
 echo "--------准备杀死-------"
 echo "-----------------------"
