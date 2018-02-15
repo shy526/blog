2933ee729c4876f0805115dadc685b389bf760d3
ccxh <sundada214@outlook.com>
2018-02-15 13:29:04 +0800

    html

--- start.sh
+++ start.sh
@@ -1,49 +1,86 @@
 #!/bin/bash
 BLOG_API_PID=`ps -ef | grep blog-api.jar  | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
 BLOG_SCHEDULER_PID=`ps -ef | grep blog-scheduler-1.0-SNAPSHOT.jar  | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
+echo "--------------------------"
+echo "------获取叛徒代号中------"
+echo "--------------------------"
 echo "blog-api.jar pid : "$BLOG_API_PID
 echo "blog-scheduler-1.0-SNAPSHOT.jar pid : "$BLOG_SCHEDULER_PID
 echo "--------------------------"
-echo "---准备杀死blog-api.jar---"
+echo "--------叛徒清理中--------"
 echo "--------------------------"
 kill -9 $BLOG_API_PID
 if [ $? -eq 0 ]
 then
-    echo "执行成功"
+    echo "成功刺杀blog-api.jar"
 else
-    echo "执行失败"
+    echo "刺杀blog-api.jar失败"
 fi
 
-kill -9 $BLOG_API_PID
 kill -9 $BLOG_SCHEDULER_PID
 if [ $? -eq 0 ]
 then
-    echo "执行成功"
+    echo "成功刺杀grep blog-scheduler-1.0-SNAPSHOT.jar"
 else
-    echo "执行失败"
+    echo "刺杀grep blog-scheduler-1.0-SNAPSHOT.jar失败"
+fi
+echo "--------------------------"
+echo "-------正在烧毁尸体-------"
+echo "--------------------------"
+rm -rf /home/project/blog-api/blog-api.jar
+if [ $? -eq 0 ]
+then
+    echo "烧毁+1"
+else
+    echo "烧毁失败"
+fi
+rm -rf /home/project/blog-scheduler/blog-scheduler-1.0-SNAPSHOT.jar
+if [ $? -eq 0 ]
+then
+    echo "烧毁+1"
+else
+    echo "烧毁失败"
 fi
 
 echo "--------------------------"
-echo "-------正在重新启动-------"
+echo "-------物色新的种子-------"
+echo "--------------------------"
+cp /home/blog/blog/blog-api/target/blog-api.jar /home/project/blog-api/
+if [ $? -eq 0 ]
+then
+    echo "种子+1"
+else
+    echo "种子-1"
+fi
+cp /home/blog/blog/blog-scheduler/target/blog-scheduler-1.0-SNAPSHOT.jar /home/project/blog-scheduler/
+if [ $? -eq 0 ]
+then
+    echo "种子+1"
+else
+    echo "种子-1"
+fi
+
+echo "--------------------------"
+echo "-------培养新的刺客-------"
 echo "--------------------------"
 nohup java -jar /home/project/blog-api/blog-api.jar > /home/project/blog-api/logs/spring.out &
 if [ $? -eq 0 ]
 then
-    echo "执行成功"
+    echo "培养blog-api.jar成功"
 else
-    echo "执行失败"
+    echo "培养blog-api.jar失败"
 fi
 
 nohup java -jar /home/project/blog-scheduler/blog-scheduler-1.0-SNAPSHOT.jar > /home/project/blog-scheduler/logs/spring.out &
 if [ $? -eq 0 ]
 then
-    echo "执行成功"
+    echo "培养blog-scheduler-1.0-SNAPSHOT.jar成功"
 else
-    echo "执行失败"
+    echo "培养blog-scheduler-1.0-SNAPSHOT.jar失败"
 fi
 
 echo "--------------------------"
-echo "---------查询pid中--------"
+echo "---------分配代号中-------"
 echo "--------------------------"
 
 BLOG_API_PID=`ps -ef | grep blog-api.jar  | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
@@ -52,20 +89,22 @@ echo "blog-api.jar pid : "$BLOG_API_PID
 echo "blog-scheduler-1.0-SNAPSHOT.jar pid : "$BLOG_SCHEDULER_PID
 
 echo "--------------------------"
-echo "-----更新blog-html--------"
+echo "--------拆除训练中心------"
 echo "--------------------------"
 rm -rf /home/project/blog-html/*
 if [ $? -eq 0 ]
 then
-    echo "清除旧文件成功"
+    echo "拆除成功"
 else
-    echo "清除旧文件失败"
+    echo "拆除失败"
 fi
-
-mv /home/blog/blog/blog-html/* /home/project/blog-html/
+echo "--------------------------"
+echo "-----建立新的训练中心-----"
+echo "--------------------------"
+cp /home/blog/blog/blog-html/* /home/project/blog-html/
 if [ $? -eq 0 ]
 then
-    echo "填入旧文件成功"
+    echo "建立成功"
 else
-    echo "填入旧文件失败"
+    echo "建立失败"
 fi
