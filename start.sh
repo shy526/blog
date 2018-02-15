7a6baa4d2e7fb81c54f7f1a133e56628fb6991ff
ccxh <sundada214@outlook.com>
2018-02-15 12:37:06 +0800

    工具页面添加一个http状态码并前端支持导出功能

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 BLOG_API_PID=`ps -ef | grep blog-api.jar  | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
 BLOG_SCHEDULER_PID=`ps -ef | grep blog-scheduler-1.0-SNAPSHOT.jar  | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
 echo "blog-api.jar pid : "$BLOG_API_PID
-echo " blog-scheduler-1.0-SNAPSHOT.jar : "$BLOG_SCHEDULER_PID
+echo "blog-scheduler-1.0-SNAPSHOT.jar pid : "$BLOG_SCHEDULER_PID
 echo "-----------------------"
 echo "--------准备杀死-------"
 echo "-----------------------"
