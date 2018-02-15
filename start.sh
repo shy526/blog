b92c1d72bb9ff028070c04760c463e2fbb135a1b
ccxh <sundada214@outlook.com>
2018-02-15 12:25:28 +0800

    测试

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+ID=`ps -ef | grep "$NAME" | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
+echo $ID
+echo "-----------------------"
+echo "--------准备杀死-------"
+echo "-----------------------"
